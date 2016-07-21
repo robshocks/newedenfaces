fbcc97eb5dfb6f990929a4ec304de4bac8e15472
Robert O Shaughnessy <robert@generate.ie>
2016-07-21 00:03:20 +0000

    fonts directory changed and character files added

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,15 @@
 
 import React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        {this.props.children}
+      <Navbar history={this.props.history} />  
+      {this.props.children}
+	    <Footer />
       </div>
     );
   }
--- server.js
+++ server.js
@@ -37,7 +37,24 @@ app.use(function(req, res) {
   });
 });
 
+/**
+ * Socket.io stuff.
+ */
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var onlineUsers = 0;
 
-app.listen(app.get('port'), function() {
+io.sockets.on('connection', function(socket) {
+  onlineUsers++;
+
+  io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+
+  socket.on('disconnect', function() {
+    onlineUsers--;
+    io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
+  });
+});
+
+server.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
